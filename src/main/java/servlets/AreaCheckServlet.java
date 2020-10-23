94d88d52370e1921bbf79c42f58f17b8be1ff87e
Kirill Kondrashov <64485121+LeartAule@users.noreply.github.com>
2020-10-23 18:44:19 +0300

    Add files via upload

---
+++ src/main/java/servlets/AreaCheckServlet.java
@@ -0,0 +1,74 @@
+package main.java.servlets;
+
+
+import main.java.models.Entries;
+import main.java.models.Entry;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/area_check")
+public class AreaCheckServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		double x = 0;
+		double y = 0;
+		double r = 0;
+		
+		try {
+			x = Utils.getDoubleParameter(request, "x");
+			y = Utils.getDoubleParameter(request, "y");
+			r = Utils.getDoubleParameter(request, "r");
+		} catch (NumberFormatException ignored) {
+		}
+		
+		boolean result = getResult(x, y, r);
+		Date currentTime = new Date();
+		
+		Entry entry = new Entry(x, y, r, result, currentTime);
+		
+		HttpSession session = request.getSession();
+		
+		Entries entries = (Entries) session.getAttribute("entries");
+		entries = entries == null ? new Entries() : entries;
+		
+		entries.addEntry(entry);
+		session.setAttribute("entries", entries);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+
+	private boolean getResult(double x, double y, double r) {
+		return inFirstQuad(x, y, r) || inSecondQuad(x, y, r) || inThirdQuad(x, y, r);
+	}
+
+	// Quadrants of coordinate system
+
+	private boolean inFirstQuad(double x, double y, double r){
+		return (x >= 0 && y >= 0 && x <= r / 2 && y <= r && x / 2 + y <= r);
+	}
+
+	private boolean inSecondQuad(double x, double y, double r){
+		return (x <= 0 && y >= 0 && y <= r && x >= -r);
+	}
+
+	private boolean inThirdQuad(double x, double y, double r){
+		return (x <= 0 && y <= 0 && (x * x + y * y <= r * r));
+	}
+
+
+	
+}
+
+
---
+++ src/main/java/servlets/Utils.java
@@ -0,0 +1,9 @@
+package main.java.servlets;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class Utils {
+	public static double getDoubleParameter(HttpServletRequest request, String parameter) {
+		return Double.parseDouble(request.getParameter(parameter).replace(",", "."));
+	}
+}
